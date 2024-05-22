f1fa7237c9312ffa81721654befca35897a2929b
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2024-05-23 03:51:16 +0700

    adding service login with verify credentials data

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -2,6 +2,7 @@ const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -32,6 +33,23 @@ class UsersService {
 
     const result = await this._pool.query(query);
     if (result.rows.length > 0) throw new InvariantError('Username sudah digunakan');
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+    const result = await this._pool.query(query);
+    if (!result.rowCount) throw new AuthenticationError('Credential yang anda berikan salah');
+
+    const { id, password: hashedPassword } = result.rows[0];
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) throw new AuthenticationError('Password anda salah');
+
+    // nilai id ini akan digunakan untuk membuat accesstoken dan refreshtoken
+    return id;
   }
 }
 
