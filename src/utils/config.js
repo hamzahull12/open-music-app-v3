906a961712449070ab85dbe5e93be69bda576234
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2024-05-23 13:22:24 +0700

    adding management token schema

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,6 +5,9 @@ const config = {
     host: process.env.HOST,
     port: process.env.PORT,
   },
+  token: {
+    accessToken: process.env.ACCESS_TOKEN_KEY,
+    refreshToken: process.env.REFRESH_TOKEN_KEY,
+  },
 };
-
 module.exports = config;
